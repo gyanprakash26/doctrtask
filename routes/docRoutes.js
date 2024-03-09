36400a9d054dd213ec0b3d267c2b5501fcb87401
gyanprakash26 <gyanprakash2698@gmail.com>
2024-03-09 20:49:53 +0530

    test

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -4,5 +4,7 @@ const docController = require('../controllers/doctorController')
 const router = express.Router();
 
 router.post('/availbe', docController.getAvailbleDoctor)
+router.post('/availbe2', docController.getAvailbleDoctor)
+
 
 module.exports = router
